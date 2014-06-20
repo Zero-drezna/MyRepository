72c8b990817ee185f14a504e675ecb5ea7c7029a
Zero-drezna <moskvin.aleksandr1991@mail.ru>
2014-06-20 13:25:15 +0400

    Create one.py

---
+++ one.py
@@ -0,0 +1,14 @@
+#открыть и считать файл с классом в список
+#Забрать название класса
+#получить список всех public методов
+import sys
+import myMethods as utils#импортируем функции под именем Utils
+inFile = open('class.txt')#файл с классом
+content = inFile.readlines()
+inFile.close()
+className = utils.getClassName(content)#Получаем имя класса
+print('Class_Name = '+className)
+methods = utils.getPublicMethodsList(content)#Получаем список имен методов класса(public, non-argument)
+for x in methods:
+	print(x)
+print('count methods = '+str(len(methods)))
