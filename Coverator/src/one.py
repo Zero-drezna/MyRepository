07592da4790bea3765acb8f417362827ad11dbfd
Zero-drezna <eldorado@MacBook-Air-eldorado.local>
2014-06-22 17:25:24 +0400

    Moskvin from Home
    added output in file module

--- Coverator/src/one.py
+++ Coverator/src/one.py
@@ -3,13 +3,15 @@
 #получить список всез public методов
 import sys
 import myMethods as utils#импортируем функции под именем utils
+import output as output#импортируем модуль вывода в файл как тест
 print ('python ver: '+sys.version[:3])#print python version
 inFile = open('class.txt')#файл с классом
 content = inFile.readlines()
 inFile.close()
 className = utils.getClassName(content)#получаем имя класса
 print('Class_Name = '+className)
-methods = utils.getPublicMethodsList(content)
+methods = utils.getPublicMethodsList(content)#получаем список методов класса
 for x in methods:
 	print(x)
 print('count methods = '+str(len(methods)))
+output.writeTest(className, methods)#выводим в файл(пишем сам тест)
